790986cde4692de34a904dcfb41f69a956e7b67d
BagchiMB <mbagchi6884@gmail.com>
2020-04-28 00:23:02 +0530

    Updates

---
+++ precache-manifest.2cd7588e6ccedc74e858ce18ceb55187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4eadcb459e48a8bcf33757d19dbbfab",
+    "url": "/Covid-19-India-Tracker/index.html"
+  },
+  {
+    "revision": "8eb2812235ba1388b4ad",
+    "url": "/Covid-19-India-Tracker/static/css/main.7c3293e2.chunk.css"
+  },
+  {
+    "revision": "dba06c8cb7a72cbe2b96",
+    "url": "/Covid-19-India-Tracker/static/js/2.ea2d22fd.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/Covid-19-India-Tracker/static/js/2.ea2d22fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb2812235ba1388b4ad",
+    "url": "/Covid-19-India-Tracker/static/js/main.ea1e860b.chunk.js"
+  },
+  {
+    "revision": "83faef7458941237ecde",
+    "url": "/Covid-19-India-Tracker/static/js/runtime-main.585395cf.js"
+  }
+]);
