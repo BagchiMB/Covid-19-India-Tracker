f639345d6aefec3879217b87f97e75e3415e3331
BagchiMB <mbagchi6884@gmail.com>
2020-04-28 00:53:27 +0530

    try

--- src/Codebase.js
+++ src/Codebase.js
@@ -129,7 +129,7 @@ function Codebase(props)
         setFinalArray(NFA)
     }
     useEffect(()=>{
-        ReactGA.pageview('/resources')
+        ReactGA.pageview('/resources') 
     },[])
     return(
         <animated.div style={Animatedprops} className={classes.root}>
